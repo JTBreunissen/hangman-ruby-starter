78efcb67626dc4ba37cb2a820b45db9f72f38dda
JTBreunissen <jantjarco@gmail.com>
2017-03-26 18:55:24 +0200

    working version

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -4,9 +4,76 @@ class Hangman
   def initialize
     @random_word = RandomWord.new
     @bad_guesses_left = 10
+    @answer_array = []
+    @bad_guesses = 0
   end
 
   def play!
-    puts "Hi from lib/hangman.rb!"
+    welcome
+    while @bad_guesses_left > 0 do
+      valid_input_print
+      puts "\n\n\n"
+      puts "Bad guesses Remaining: #{@bad_guesses_left}"
+      puts "\n\n"
+      get_user_answer
+      evaluation
+      display_guessword
+      if word_guessed?
+        puts "Congratulations, you beat the hangman"
+        puts "The word was #{@random_word.word_array.join("").upcase}"
+        break
+      end
+    end
+    if !word_guessed?
+      puts "\n\n\n"
+      puts "Today is not your lucky day: The Hangman got you!"
+      puts "\n\n\n"
+    end
   end
+
+    def welcome
+      puts "********************************************************************"
+      puts "*********************Welcome to HANGMAN*****************************"
+      puts "*You have 10 lives, each time you guess a wrong letter, you lose 1.*"
+      puts "*************When you reach 0 lives, it's GAME OVER*****************"
+      puts "********************************************************************"
+      puts "\n "
+    end
+
+    def valid_input_print
+      puts "valid input for guess: "
+      RandomWord::VALID_INPUT.each do |x|
+        print "#{x} "
+      end
+    end
+
+    def display_guessword
+      puts "This is what the word looks like now: "
+      puts "#{@random_word.positions_for(characters: @answer_array)}"
+    end
+
+    def get_user_answer
+      puts "What will be your guess? what will be your pick? \n   Better hurry up now, cause the rope is quick! "
+      puts "\n\n"
+      print "Your guess: "
+      @answer = gets.chomp
+    end
+
+    def evaluation
+      unless @random_word.word.include?(@answer)
+      puts "Sadly, we are not looking for that letter"
+      lose_life
+      else
+        @answer_array << @answer
+        @random_word.positions_for(characters: @answer_array)
+      end
+    end
+
+    def word_guessed?
+      @random_word.contains?(@answer_array)
+    end
+
+    def lose_life
+      @bad_guesses_left -= 1
+    end
 end
--- lib/random_word.rb
+++ lib/random_word.rb
@@ -1,13 +1,49 @@
 class RandomWord
-  attr_reader :word
+  attr_reader :word, :word_array, :answer_array
 
-  WORDS = %w(programming monkeybusiness rubyesque styleguide)
+  WORDS = %w(test)
+  VALID_INPUT = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K",
+  "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
 
   def initialize
     @word = WORDS.sample
+    @word_array = @word.chars.to_a
+  end
+
+  def setup_guessword
+    word_array.each do |c|
+      print "_,"
+    end
   end
 
   def positions_for(characters: [])
-    # for you to implement :)
+    pos = position_setup
+      for c in 0...pos.length
+      for a in 0...characters.length
+      pos[c] = characters[a] if self.word[c] == characters[a]
+      end
+    end
+    return pos
   end
+
+  def position_setup
+    self.word.split(//).map{|c| ""}
+  end
+
+  def amount_bad_guesses(characters)
+    bad_guesses=0
+    bad_guess_array = characters - @word_array
+    @bad_guesses = bad_guess_array.count
+    return @bad_guesses
+  end
+
+  def contains? (player_selections)
+    gamesword = Set.new(self.word.split(//))
+    playerinput = Set.new(player_selections)
+    gamesword.subset?(playerinput)
+  end
+
+
+
 end
